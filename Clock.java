0eba7ea6902fac2e6b86ec0f203ceeb4d6dc58fa
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 13:31:45 -0700

    Update Clock.java

--- Clock.java
+++ Clock.java
@@ -1,4 +1,7 @@
-//Ananya Heroor, 9074986762, heroor
+/** 
+* This class determines the angle of the hands of the clock
+*
+*/
 public class Clock{
     public static void main (String [] args){
         //computeSecondHandAngle(0);
@@ -26,12 +29,35 @@ public class Clock{
         //computeHourHandAngle(18);
         // System.out.println("The hour hand angle is "+computeHourHandAngle(18)+ " degrees");
     }
+/** 
+* This function determines the angle of the second hand
+* @param int seconds between 0 and 59 inclusive
+* @return int representing angle in degrees of second hand when current time has seconds seconds passed
+* 0 degrees is 12 o*clock
+* Degrees increase counterclockwise (3 o'clock is 90 degrees)
+*/
     public static int computeSecondHandAngle (int seconds){
         return (seconds*6);
     }
+/** 
+* This function determines the angle of the minute hand
+* @param int minutes between 0 and 59 inclusive
+* @return int representing angle in degrees of minute hand when current time has minutes minutes, ignoring
+*   contributions from seconds
+* 0 degrees is 12 o*clock
+* Degrees increase counterclockwise (3 o'clock is 90 degrees)
+*/
     public static int computeMinuteHandAngle (int minutes){
         return (minutes*6);
     }
+ /** 
+* This function determines the angle of the hour hand
+* @param int hours between 0 and 23 inclusive
+* @return int representing angle in degrees of hour hand when current time has hours hours 
+*   ignoring contributiosn from minutes and seconds
+* 0 degrees is 12 o*clock
+* Degrees increase counterclockwise (3 o'clock is 90 degrees)
+*/
     public static int computeHourHandAngle (int hours){
         return ((hours%12)*30);
     }
